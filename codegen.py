ae17148a6a6cbd8b304e8b10bf6430cf428ec48c
Clément Bœsch <u@pkh.me>
2014-06-07 17:53:06 +0200

    codegen: generate real c code

--- codegen.py
+++ codegen.py
@@ -22,7 +22,7 @@ def create_ast(dim):
 
         extradiff_cond = noextradiff_cond = None
         for cond, permuts in interp_cond_permuts:
-            ast_cond = ['or']
+            ast_cond = ['||']
             for enabled_dots, disabled_dots in permuts:
                 mask_diff = mask_nodiff = 0
                 for dot in enabled_dots:
@@ -31,14 +31,18 @@ def create_ast(dim):
                     mask_nodiff |= 1<<ppos[::-1].index(dot)
                 mask_values_that_matter = mask_diff | mask_nodiff
                 ast_cond.append('(k&0x%02x) == 0x%02x' % (mask_values_that_matter, mask_diff))
+
+            if len(ast_cond) == 2:
+                ast_cond = ast_cond[1]
+
             if cond:
                 diff_func = 'yuv_diff(w[%d], w[%d])' % (cond[0], cond[1])
-                extradiff_cond = ['and', diff_func, ast_cond]
+                extradiff_cond = ['&&', diff_func, ast_cond]
             else:
                 noextradiff_cond = ast_cond
 
         if extradiff_cond and noextradiff_cond:
-            ast_cond = ['or', extradiff_cond, noextradiff_cond]
+            ast_cond = ['||', extradiff_cond, noextradiff_cond]
         elif extradiff_cond:
             ast_cond = extradiff_cond
         elif noextradiff_cond:
@@ -48,7 +52,7 @@ def create_ast(dim):
 
         if not root_cond:
             root_cond = entry_cond = ['if', ast_cond, 'PIXEL%s' % interpid, None]
-        elif i == len(interp_cond_permuts) - 1:
+        elif i == len(interps) - 1:
             assert entry_cond[3] is None
             entry_cond[3] = 'PIXEL%s' % interpid
         else:
@@ -58,12 +62,35 @@ def create_ast(dim):
 
     return root_cond
 
+def get_c_code(node, need_protective_parenthesis=True):
+
+    code = []
+
+    if not isinstance(node, list):
+        return node
+
+    if node[0] == 'if':
+        code.append('if (%s)' % get_c_code(node[1], need_protective_parenthesis=False))
+        code.append(' ' * 4 + get_c_code(node[2]))
+        code.append('else')
+        c_code = get_c_code(node[3])
+        if c_code.startswith('if'):
+            code[-1] += ' ' + c_code
+        else:
+            code.append(' ' * 4 + c_code)
+        return '\n'.join(code)
+
+    assert node[0] in ('||', '&&')
+    c_code = (' %s ' % node[0]).join(get_c_code(x) for x in node[1:])
+    return '(%s)' % c_code if need_protective_parenthesis else c_code
+
 def main():
     dim = int(sys.argv[1])
 
     root_cond = create_ast(dim)
+    code = get_c_code(root_cond) + '\n'
 
-    open('hq%dx_tpl.c' % dim, 'w').write(pprint.pformat(root_cond, width=200))
+    open('hq%dx_tpl.c' % dim, 'w').write(code)
 
 if __name__ == '__main__':
     main()
